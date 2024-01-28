656c3388fdd73af5ee5664fdbd4bf587f65b8ad8
zflorezz <zakinahf030904@gmail.com>
2024-01-28 17:02:59 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>  
+import {ref} from 'vue'
+
+const name = ref('')
+</script>
+
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="name" v-model="name" />
+        <!-- {{ name }} -->
+      </h2>
+    </section>
+
+    <section class="create-todo">
+
+    </section>
+
+    <section class="todo-list">
+
+    </section>
+  </main>
+</template>
