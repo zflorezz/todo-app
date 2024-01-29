c71c07062a6e196b9cf4284731b0e579ee7cedc9
zflorezz <zakinahf030904@gmail.com>
2024-01-28 23:38:32 -0600

    todo list displaying

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ const addTodo = () => {
   todos.value.push({
     content: input_content.value,
     category: input_category.value,
+    done: false,
   })
 
  input_content.value = ''
@@ -56,11 +57,25 @@ const addTodo = () => {
 
           <!-- {{ input_category }} -->
         </div>
+
+        <input type="submit" value="Add Todo" />
+
       </form>
     </section>
 
     <section class="todo-list">
-
+      <div class="list">
+        <div v-for="todo in todos" :class="`todo-item ${todo.done ? `done` : `not-done`}`" :key="todo">
+        <label>
+          <input type="checkbox" v-model="todo.done" />
+        </label>
+        <span :class="`bubble ${todo.category}`"></span>
+        <div class="todo-content">
+          <input type="text" v-model="todo.content" />
+        </div>
+        
+        </div>
+      </div>
     </section>
   </main>
 </template>
